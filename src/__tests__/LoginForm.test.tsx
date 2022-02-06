d2ad3030f9b6c790e9b6fa0db796afbe67369b66
vykis123 <vykintaspilipauskas@gmail.com>
2022-02-06 20:52:33 +0200

    tests

--- src/__tests__/LoginForm.test.tsx
+++ src/__tests__/LoginForm.test.tsx
@@ -1,26 +1,26 @@
-// import React from "react";
-// import { render, fireEvent, screen } from "@testing-library/react";
-// import userEvent from "@testing-library/user-event";
-// import Login from "../components/Login/Login";
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Login from "../components/Login/Login";
 
-// describe("<Login />", () => {
-//   test("should check if username input is present", async () => {
-//     render(<Login />);
+describe("<Login />", () => {
+  test("should check if username input is present", async () => {
+    render(<Login />);
 
-//     const inputEl = screen.getByPlaceholderText("Username");
-//     expect(inputEl).toBeInTheDocument();
-//     expect(inputEl).toHaveValue("username");
-//   });
+    const inputEl = screen.getByPlaceholderText("Username");
+    expect(inputEl).toBeInTheDocument();
+    expect(inputEl).toHaveValue("username");
+  });
 
-//   test("testing valid email to test email input field", () => {
-//     render(<Login />);
+  test("testing valid email to test email input field", () => {
+    render(<Login />);
 
-//     const inputEl = screen.getByPlaceholderText("Username");
-//     userEvent.type(inputEl, "test@mail.com");
+    const inputEl = screen.getByPlaceholderText("Username");
+    userEvent.type(inputEl, "test@mail.com");
 
-//     expect(screen.getAllByPlaceholderText("Username")).toHaveValue(
-//       "test@mail.com"
-//     );
-//     expect(screen.queryByTestId("error")).not.toBeInTheDocument();
-//   });
-// });
+    expect(screen.getAllByPlaceholderText("Username")).toHaveValue(
+      "test@mail.com"
+    );
+    expect(screen.queryByTestId("error")).not.toBeInTheDocument();
+  });
+});
